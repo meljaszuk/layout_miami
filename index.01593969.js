a024c551840c21123d52a62407fec8d5b076f161
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-01-14 17:10:50 +0100

    make build

---
+++ index.01593969.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")}),document.addEventListener("DOMContentLoaded",function(){document.getElementById("form").reset()});
+//# sourceMappingURL=index.01593969.js.map
